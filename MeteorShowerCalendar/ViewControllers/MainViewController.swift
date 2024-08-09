dd94ee90fdb181067f7935eef6db1f611eeef898
That Guy Vic <vicsergeevrov@icloud.com>
2024-08-09 21:29:08 +0300

    no storyboard template created
    preparing for using programmatic UI

---
+++ MeteorShowerCalendar/ViewControllers/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MeteorShowerCalendar
+//
+//  Created by Victor on 09.08.2024.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    
+    var tableView = UITableView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
